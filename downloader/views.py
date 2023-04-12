815506d150e7c03771c58992b5cfc661f044b354
Partho Debnath <parthodebnath28@gmail.com>
2023-04-13 03:26:17 +0600

    added audio file download option and css

--- downloader/views.py
+++ downloader/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.conf import settings
+from django.http import FileResponse, HttpResponseBadRequest
 from pathlib import Path
 
 from pytube import YouTube
@@ -15,20 +16,27 @@ class HomeView(View):
         return render(request, self.template_name)
     
     def post(self, request, *args, **kwargs):
-        convertion_type = request.POST.get('convertion-type')
+        
         url = request.POST.get('url')
-        yt = YouTube(url)
+        # convertion_type = request.POST.get('convertion-type')
 
-        # for item in yt.streams.filter(only_audio=True):
-        #     print(item.abr)
-        #     print(item.url)
+        try:
+            yt = YouTube(url)
+            dc = dict()
+            for item in yt.streams.filter(only_audio=True):
+                # print(item.abr)
+                # print(item.url)
+                dc[item.abr.replace('kbps', '')] = item.itag
+            maxKbps = max([int(i) for i in dc.keys()])
+            bestQualityItag = dc[f'{maxKbps}']
+            dc.clear()
 
-        stream = yt.streams.filter(only_audio=True).first()
-        dire = Path.joinpath(settings.BASE_DIR, 'medias')
-        a = stream.download(dire)
-    
-        with open(a, 'rb') as file:
-            data = file.read()
+            # stream = yt.streams.filter(only_audio=True).first()
+            # dire = Path.joinpath(settings.BASE_DIR, 'medias')
 
-        context = {'url': data}
-        return render(request, self.template_name, context)
+            # a = stream.download(dire)
+            # with open(a, 'rb') as file:
+            #     data = file.read()
+        except Exception as e:
+            return HttpResponseBadRequest('Please Enter Youtube Video URL.')
+        return FileResponse(open(yt.streams.get_by_itag(bestQualityItag).download(), 'rb'), as_attachment=True)
